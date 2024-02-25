bd3a6b91def22b0e2879fbb13bc21372b94a42ce
Tom Warner <tomwamt@gmail.com>
2024-02-25 01:30:22 -0700

    v2 Fix unclosed db connections

--- service/src/main/kotlin/budget/db/Database.kt
+++ service/src/main/kotlin/budget/db/Database.kt
@@ -1,13 +1,13 @@
 package budget.db
 
 import org.jooq.DSLContext
+import org.jooq.SQLDialect
 import org.jooq.impl.DSL
 import javax.sql.DataSource
 
 class Database(private val dataSource: DataSource) {
     fun <T> transaction(block: (DSLContext) -> T): T {
-        val connection = dataSource.connection
-        val dsl = DSL.using(connection)
+        val dsl = DSL.using(dataSource, SQLDialect.POSTGRES)
         return dsl.transactionResult { cfg -> block(cfg.dsl()) }
     }
 }
